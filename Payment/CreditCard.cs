bb8429147b30e01f2130e212483d3ac26be31e63
Jacob Keen <jaclkeen@gmail.com>
2016-10-06 14:10:55 -0500

    C sharp order and payment system

---
+++ Payment/CreditCard.cs
@@ -0,0 +1,21 @@
+using Bangazon.Orders;
+
+namespace Bangazon.Payments
+{
+  public class CreditCard: Payment 
+  {
+
+    public string bankName { get; set; }
+    public string accountNumber { get; set; }
+
+    public CreditCard(Order order): base(order)
+    {
+
+    }
+    public override string process()
+    {
+      return $"You are using a {this.bankName} card, with the account number {this.accountNumber}\n{base.process()}";
+      
+    }
+  }
+}
